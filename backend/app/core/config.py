2c5c28f6b65e176aea345ed202b0d708e9a53b31
Muhammad Al Farizi <ziziyusrizal@gmail.com>
2025-09-23 17:08:53 +0200

    feat: Set up CORS URL in config and handle .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,29 +11,16 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Qubitkit Backend"
     VERSION: str = VERSION
     API_V1_STR: str = "/api/v1"
+
     # Configurable settings
     DEBUG: bool = True
     HOST: str = "0.0.0.0"
     PORT: int = 8000
-
-    DOMAIN: str = "localhost:8000"
-    FRONTEND_DOMAIN: str = "localhost:3000"
+    FRONTEND_URL: str
 
     @property
     def BACKEND_CORS_ORIGINS(self) -> List[str]:
-        if self.DEBUG:
-            return [
-                f"http://{self.FRONTEND_DOMAIN}",
-                f"https://{self.FRONTEND_DOMAIN}",
-                "http://localhost:3000"
-            ]
-        else:
-            return [f"https://{self.FRONTEND_DOMAIN}"]
-
-    @property
-    def API_URL(self) -> str:
-        protocol = "http" if self.DEBUG else "https"
-        return f"{protocol}://{self.DOMAIN}{self.API_V1_STR}"
+        return [self.FRONTEND_URL]
 
     class Config:
         env_file = ".env"
